bc586ca7447d6fa660959e1b2d5cee22ea380ec8
gon <gonzad2001@hotmail.com>
2026-04-23 00:59:25 -0300

    update tick format

--- js/plots/plotsUtils/tickFormat.js
+++ js/plots/plotsUtils/tickFormat.js
@@ -2,27 +2,17 @@
  * function that takes a float and returns its corresponding string format
  */
 export function customTickFormat(d) {
-    return d;
+    const s = String(d).replace(/[-.]/g, "");
 
-    // if (d < 0) {
-    //     // Format small negative numbers in scientific notation
-    //     if (d > -1) {
-    //         return d.toExponential(1);  // Scientific notation for small negative numbers
-    //     } else if (String(d).length > 5) {
-    //         return d.toExponential(1);  // Format large negative numbers in scientific notation
-    //     } else {
-    //         return d;  // Display as is
-    //     }
-    // } else {
-    //     if (d > 0 && d < 1) {
-    //         return d.toExponential(1);
-    //     } else if (d === 0) {
-    //         return "0";  // Display zero without decimal
-    //     } else if (String(d).length > 5) {
-    //         return d.toExponential(1);
-    //     } else {
-    //         return d;
-    //     }
-    // }
+    if (s.length <= 5) {
+        // truncate to 5 significant digits
+        return Number(d)
+            .toPrecision(5)
+            .replace(/\.?0+$/, "");
+    }
+
+    // scientific notation with fixed significant digits
+    return Number(d).toExponential(1);
 }
+
 
